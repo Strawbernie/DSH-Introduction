8fb0e7930308a2e96ba7254227777dd5c237c0b1
Strawbernie <wijbengabernard1@gmail.com>
2023-12-06 17:02:52 +0100

    implemented slicing
    object to slice needs the "Sliceable" Layermask

---
+++ Assets/Slice/SliceObject.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+using UnityEngine.InputSystem;
+using UnityEngine.Assertions.Must;
+
+public class SliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public VelocityEstimator velocityEstimator;
+    public LayerMask sliceableLayer;
+    public Material CrossMaterial;
+    public float cutforce = 2000f;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        bool hasHit= Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
+        if (hasHit)
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if (hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, CrossMaterial);
+            SetupSlicedComponent(upperHull);
+            GameObject lowerHull = hull.CreateLowerHull(target, CrossMaterial);
+            SetupSlicedComponent(lowerHull);
+
+            Destroy(target);
+        }
+    }
+    public void SetupSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider> ();
+        int LayerSliceable = LayerMask.NameToLayer("Sliceable");
+        slicedObject.layer = LayerSliceable;
+        collider.convex = true;
+        rb.AddExplosionForce(cutforce, slicedObject.transform.position, 1);
+    }
+}
