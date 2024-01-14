c974212d6a9b79f68b0531880e1223ecd30afd4d
heyitsrenn <itsrennsmail@gmail.com>
2024-01-14 16:52:33 +0100

    Fixed issues & added ontriggerexit
    fixed issues with the ontriggerenter and made it so the dialogue initiation button disappears once you walk away from the NPC

--- Assets/Scripts/DialogueSystem/DialogueTrigger.cs
+++ Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -8,15 +8,21 @@ public class DialogueTrigger : MonoBehaviour
     public Canvas buttonCanvas;
     private void Awake()
     {
-        //buttonCanvas.gameObject.SetActive(false);// made it so it can be tested without VR 
+        buttonCanvas.gameObject.SetActive(false);// made it so it can be tested without VR 
     }
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collider)
     {
+        Debug.Log("It does work");
         buttonCanvas.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        buttonCanvas.gameObject.SetActive(false);
     }
 }
 
