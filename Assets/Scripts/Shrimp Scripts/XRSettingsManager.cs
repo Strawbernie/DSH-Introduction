f56b6397378156f1ed065fc63c4887bd77cd2d74
Strawbernie <127209681+Strawbernie@users.noreply.github.com>
2024-01-17 14:39:54 +0100

    added arm calculation

--- Assets/Scripts/Shrimp Scripts/XRSettingsManager.cs
+++ Assets/Scripts/Shrimp Scripts/XRSettingsManager.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+using UnityEngine.XR;
 
 public class XRSettingsManager : MonoBehaviour
 {
     public static event Action XRSettingsChange;
     public static XRSettingsManager Instance;
-
+    private InputDevice RightController;
+    public GameObject leftController;
+    public GameObject rightController;
+    public InputData inputData;
     public bool _continuousTurnActive = false;
     public bool _vignetteActive = false;
     public bool _teleportActive = false;
+    float distance;
+    private void Start()
+    {
+        RightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+    }
+    private void Update()
+    {
+        CheckControllerInput(RightController);
+    }
     private void Awake()
     {
         if(Instance == null)
@@ -59,5 +72,16 @@ public class XRSettingsManager : MonoBehaviour
     public bool isTeleportActive()
     {
         return _teleportActive;
+    }
+    private void CheckControllerInput(InputDevice controller)
+    {
+        if (inputData._rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool AButton))
+        {
+            if (AButton)
+            {
+               distance = Vector3.Distance (rightController.transform.position, leftController.transform.position);
+                Debug.Log("" + distance);
+            }
+        }
     }
 }
